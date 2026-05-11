26fcf83d0a55f0127d8a452e475aded884716b19
Antoine Romand <antoine.romand@epitech.eu>
2026-05-11 15:22:04 +0200

    feat: fix project orientation

--- portofolio-app/src/app/components/reusable/project/project-component.ts
+++ portofolio-app/src/app/components/reusable/project/project-component.ts
@@ -11,6 +11,7 @@ import {IdFormatterPipe} from '../../../pipes/id-formatter-pipe';
 })
 export class ProjectComponent {
   project = input.required<ProjectType>();
+  displayIndex = input.required<number>();
   defaultImgSrc =
     'https://lh3.googleusercontent.com/aida-public/AB6AXuCQ_MQlv6kK2EWiD48KlM33C4I0Ftt9Ix88s8Mxsl3rCkQYKZST0JcQDHOxeRF6uUQnGYKd9ui5F9rhOiFeBlJsJXvjv_SYCAwKenlhQs-eKnw03_Z1nBL1zfiA85mtJ8RrsrsNpXZoRvWFhtRBS25Jc9TcxMiDo1hqqAlu_i4akSGRQnSHz8hI5AISrDVQ11v_sLvGw7vZPqAuYXkJAgG1g7-m1yX8UeFzuVQ0VIkH4HGRADxYZLOFn0BdsgvodurDz3TwMYU0MfA';
 
@@ -25,8 +26,8 @@ export class ProjectComponent {
     }
   }
 
-  isEven(id: number | undefined) {
-    if (id && id % 2 === 0) return true;
-    return false;
+  shouldReverse(index: number | undefined) {
+    if (index === undefined) return false;
+    return index % 2 === 1;
   }
 }
